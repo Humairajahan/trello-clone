6d4efe57a214f36235277a95cb61e37ed2af2059
Humairajahan <humairazim45@gmail.com>
2025-09-25 19:02:45 +0600

    Handle common exception cases globally

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   protect_from_forgery with: :null_session
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+
+  private
+  def not_found(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :not_found
+  end
+
+  def not_destroyed(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_content
+  end
 end
